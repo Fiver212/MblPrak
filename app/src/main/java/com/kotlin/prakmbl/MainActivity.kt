2e8ca01157995c56a905eb9f16b6bfdc46b0810b
Figo Veron de Fretes <figo.veron@students.utdi.ac.id>
2022-09-23 14:39:42 +0700

    push prak3

---
+++ app/src/main/java/com/kotlin/prakmbl/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kotlin.prakmbl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
